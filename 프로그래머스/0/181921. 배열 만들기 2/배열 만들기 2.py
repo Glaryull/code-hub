f78d185fe3a669a2a5525b5168bd974f3a56c60a
glaryull <90367250+Glaryull@users.noreply.github.com>
2024-08-10 09:53:28 +0900

    [level 0] Title: 배열 만들기 2, Time: 469.48 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,8 @@
+def solution(l, r):
+    answer = []
+    for i in range(l,r+1):
+        if all(li in ['0','5'] for li in str(i)):
+            answer.append(i)
+    if len(answer)==0:
+        answer.append(-1)  
+    return answer 
