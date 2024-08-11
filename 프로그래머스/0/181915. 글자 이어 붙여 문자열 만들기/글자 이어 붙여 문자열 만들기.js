8f430d7303452ab4887c8bd418f831a3a225eeee
glaryull <90367250+Glaryull@users.noreply.github.com>
2024-08-11 12:20:03 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.31 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -0,0 +1,10 @@
+function solution(my_string, index_list) {
+    var answer = "";
+    
+        for (let i of index_list){ 
+            answer += my_string[i];
+            
+        
+    }
+    return answer;
+}
