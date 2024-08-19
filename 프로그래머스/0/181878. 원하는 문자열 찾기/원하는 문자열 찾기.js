4e3e3a08b85ea4d51771ba82558a72f80751a753
glaryull <90367250+Glaryull@users.noreply.github.com>
2024-08-19 21:44:23 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.12 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -0,0 +1,12 @@
+function solution(myString, pat) {
+    var answer = 0;
+    
+
+    if(myString.toUpperCase().indexOf(pat.toUpperCase())>=0){
+        return 1
+    }
+    else
+        return 0
+    
+    
+}
