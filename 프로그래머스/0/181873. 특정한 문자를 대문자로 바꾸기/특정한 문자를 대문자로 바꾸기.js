db8f9e94fffe931194cdd01b91772877b3bcb35c
glaryull <90367250+Glaryull@users.noreply.github.com>
2024-08-28 01:11:54 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -0,0 +1,10 @@
+function solution(my_string, alp) {
+    var answer = '';
+    answer = [...my_string]; 
+    for(let i=0;i<answer.length;i++){
+        if( answer[i] === alp){
+            answer[i] = answer[i].toUpperCase();
+        }
+    }
+    return answer.join('');
+}
